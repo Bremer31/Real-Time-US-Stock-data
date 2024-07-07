6fe75b4a3c45b20ee0fd0f7816303dfbf4dfa1d8
Bremer31 <97674557+Bremer31@users.noreply.github.com>
2024-07-08 02:03:27 +0300

    Add files via upload

---
+++ example_code.py
@@ -0,0 +1,21 @@
+from Stock_Data import * 
+
+
+company_prices = company_data_price("AAPL")
+company_financial = company_financials("AAPL")
+
+ema200 = company_financial.twohundred_ema()
+ema50 = company_financial.fifty_ema()
+revenue_per_share = company_financial.revenue_per_share()
+fiscal_year_end = company_financial.find_fiscal_year_end()
+profit_margin = company_financial.profit_margin()
+operating_margin = company_financial.operating_margin()
+
+
+
+
+price = company_prices.find_stock_price()
+avg_volume = company_prices.find_avg_volume()
+market_cap=company_prices.find_marketcap()
+
+
